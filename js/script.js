0371018917bad19ab40109dbede9675b56c99949
Abay <abayibrayev7@gmail.com>
2019-12-27 22:42:29 +0000

    added a POST httprequests to the main-form

--- js/script.js
+++ js/script.js
@@ -109,17 +109,68 @@ window.addEventListener('DOMContentLoaded', function() {
       overlay = document.querySelector('.overlay'),
       close = document.querySelector('.popup-close');
       
-  more.addEventListener('click', () => {
+  more.addEventListener('click', function() {
     overlay.style.display = 'block';
     this.classList.add('more-splash');
     document.body.style.overflow = 'hidden';
   });
 
-  close.addEventListener('click', () => {
+  close.addEventListener('click', function() {
     overlay.style.display = 'none';
     more.classList.remove('more-splash');
     document.body.style.overflow = '';
   });
 
+/* ---------------------------------- Form ---------------------------------- */
 
+  let message = {
+      loading: 'Loading...',
+      success: 'Thank you! We will contact you soon',
+      failure: 'Something went wrong!'
+  };
+
+  let form = document.querySelector('.main-form'),
+      input = form.getElementsByTagName('input'),
+      statusMessage = document.createElement('div');
+
+      statusMessage.classList.add('status');
+  
+  form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      form.appendChild(statusMessage);
+
+      let request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+      // request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded');
+      request.setRequestHeader ('Content-type', 'application/json; charset=utf-8');
+
+
+      let formData = new FormData(form);
+
+      //sends form data in a JSON format
+      let obj = {};
+      formData.forEach(function(value, key) {
+        obj[key] = value;
+      });
+      let json = JSON.stringify(obj);
+
+      request.send(json);
+
+
+      request.send(formData);
+
+      request.addEventListener('readystatechange', function() {
+        if (request.readyState < 4) {
+          statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+          statusMessage.innerHTML = message.success;
+        } else {
+          statusMessage.innerHTML = message.failure;
+        }
+      });
+
+        for (let i = 0; i < input.length; i++) {
+          input[i].value = '';
+        }
+  });
 });
